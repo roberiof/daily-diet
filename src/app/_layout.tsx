ed81a28798ec172efe637792329ad0bd91996d26
Robério Filho <antonioroberiofilho8@gmail.com>
2024-09-10 19:32:15 -0300

    dfsa

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -19,6 +19,7 @@ export {
   ErrorBoundary
 } from "expo-router";
 
+
 export const unstable_settings = {
   // Ensure that reloading on `/modal` keeps a back button present.
   initialRouteName: "(public)"
